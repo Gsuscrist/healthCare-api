149486ef132ef4f202703dedd3a7ebff1f8e1a07
Gsus <213359@ids.upchiapas.edu.mx>
2023-03-29 20:13:46 -0600

    [delete] build folder

--- build/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _mongoose = _interopRequireDefault(require("mongoose"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_mongoose["default"].connect("mongodb+srv://healthCare:secureHealth@healthcare.zlay3ab.mongodb.net/?retryWrites=true&w=majority").then(function (db) {
-  return console.log("database connection execute successfully");
-})["catch"](function (error) {
-  return console.log(error);
-});
